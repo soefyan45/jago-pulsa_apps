d6afe83d1f21a0f7c42a67514192f74b61263e44
soefyan syah <soefyan45@gmail.com>
2024-08-03 08:33:43 +0700

    bottom menu

---
+++ components/BottomMenu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="bottom-0 fixed w-full flex-col h-[64px] items-start">
+        <div class="h-full w-full bg-biru1 flex justify-between p-2 px-20">
+            <NuxtLink to="/apps/" class="flex flex-col space-y-1 items-center">
+                <div class="h-[32px] w-[32px] bg-abu5 rounded-lg flex justify-center p-1 shadow-md" :class="{' bg-gold3':beranda==route['path']}">
+                    <IconHome class="text-biru1"/>
+                </div>
+                <span class="text-12 font-normal text-abu5">Beranda</span>
+            </NuxtLink>
+            <NuxtLink to="/apps/riwayat" class="flex flex-col space-y-1 items-center">
+                <div class="h-[32px] w-[32px] bg-abu5 rounded-lg flex justify-center p-1 shadow-md" :class="{' bg-gold3':riwayat==route['path']}">
+                    <IconRiwayat class="text-biru1"/>
+                </div>
+                <span class="text-12 font-normal text-abu5">Riwayat</span>
+            </NuxtLink>
+            <NuxtLink to="/apps/profile" class="flex flex-col space-y-1 items-center">
+                <div class="h-[32px] w-[32px] bg-abu5 rounded-lg flex justify-center p-1 shadow-md" :class="{' bg-gold3':profile==route['path']}">
+                    <IconProfile class="text-biru1"/>
+                </div>
+                <span class="text-12 font-normal text-abu5">Profile</span>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const config = useRuntimeConfig();
+	const router = useRouter();
+	const route = useRoute()
+	let beranda = '/apps/'
+	let riwayat = '/apps/riwayat'
+	let profile = '/apps/profile'
+    console.log(route['path'])
+    // console.log(route['name'])
+    // async()=>{
+	// 	console.log(val)
+	// 	var re = val.replace(/\./g, "");
+	// 	return formJumlahDepo.value = re
+	// }
+    // console.log(router)
+</script>
+
+<style>
+
+</style>
---
+++ pages/apps/profile/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nuxt-layout :name="layout" class="bg-red-800">
+            <div class="w-full flex flex-col">
+                <div class="w-full flex justify-between px-6 py-4 bg-biru1">
+                    <div class="w-8/12 flex flex-col">
+                        <span class="text-19 text-white font-semibold">Profile</span>
+                    </div>
+                </div>
+            </div>
+            <BottomMenu/>
+        </nuxt-layout>
+    </div>
+</template>
+<script setup>
+    let layout = ref('mobile-unauthorize')
+	definePageMeta({
+		middleware: [
+			'un-auth',
+		],
+	});
+</script>
+<style scoped>
+</style>
